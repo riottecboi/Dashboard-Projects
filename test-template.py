47e868d6c19d97b35d5c40f951ed216df2442c63
tranvinhliem <liem@dfir.vn>
2021-10-20 22:40:51 +0700

    Added login template + login route test

--- test-template.py
+++ test-template.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+@app.route("/login")
+def login():
+    return render_template("BsLam/login.html")
+
 @app.route("/")
 def index():
     return render_template("BsLam/index.html")
